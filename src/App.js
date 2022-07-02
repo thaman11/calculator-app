b1fa150452df9b39bcb427005aca1dd2b63f93c7
thaman11 <thaman686@gmail.com>
2022-07-02 15:56:01 +0530

    Learning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import InputBox from './components/InputBox';
+import Button from './components/CustomButton';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button value={5} />
+                 
     </div>
   );
 }
